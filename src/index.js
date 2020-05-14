6336ca9d443782d56c45bd27388a3808b8b36da5
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-05-14 16:01:01 -0400

    elliott

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 let addToy = false;
-
-const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+const url = 'http://localhost:3000/toys';
 
 
 document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector("#new-toy-btn");
+  const toyFormContainer = document.querySelector(".container");
   const toyCollection = document.querySelector('#toy-collection')
 
-  fetch('http://localhost:3000/toys')
+  fetch(url)
     .then(resp => resp.json())
     .then(toys => {
       for(const toy of toys) {
@@ -24,7 +24,46 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     })
   
+  const likeBtn = document.getElementsByClassName('like-btn')
+  console.log(likeBtn)
+  // const likeBtnArray = Array.from(likeBtn)
+  // console.log(likeBtnArray)
+  for (const btn of likeBtn) {
+    // console.log(btn)
+    btn.addEventListener('click', (event) => {
+      console.log(event.target)
+      if(event.target.className == 'like-btn'){
+        const parent = event.target.parentElement
+        const p = parent.querySelector('p')
+        const totalNumLikes = parseInt(p.textContent)
+        const newNumLikes = totalNumLikes + 1
+        p.textContent = newNumLikes
+      }
+    })
+  }
+
+  // clicks on addnewtoy
+  // make post request to add the toy
+  // render on page
   addBtn.addEventListener("click", () => {
+    // want to make a post request
+    // doesn't add to bottom of page tho
+    fetch(url, {
+      method: 'POST',
+      headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "name": "Jessie",
+        "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+        "likes": 0
+      })
+    }).then(response => response.json())
+    .then(json => {
+      toyCollection.append(json)
+    })
+
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
