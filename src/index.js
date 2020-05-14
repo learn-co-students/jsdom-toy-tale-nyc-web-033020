54d00bc1d0f4c8d8635db9e7f6528fcaf37f0cd6
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-05-14 17:02:40 -0400

    ninety-nine percent finished

--- src/index.js
+++ src/index.js
@@ -12,4 +12,75 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+
+  const allToys = 'http://localhost:3000/toys'
+  fetch(allToys)
+  .then(response => response.json())
+  .then(toys =>{
+    toys.forEach(toy => renderToy(toy))
+  })
+  function renderToy(toy){
+    const container = document.querySelector('#toy-collection')
+    container.innerHTML += `
+    <div class="card" data-num="${toy.id}">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p>${toy.likes} Likes </p>
+      <button class="like-btn">Like <3</button>
+    </div>
+    `  
+  }
+  const form = document.querySelector('.add-toy-form')
+  form.addEventListener('submit', function(e){
+    event.preventDefault()
+    const newToys = 'http://localhost:3000/toys'
+    fetch(newToys, {
+      method: 'POST',
+      headers: 
+      {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "name": form.name.value,
+        "image": form.image.value,
+        "likes": 0
+      })
+      // form.reset()
+    })
+    
+  })
+  // const card = document.getElementsByClassName('card')
+  // const toyArray = Array.from(card)
+  // console.log(toyArray)
+  // toyArray.forEach(toy => {
+  //   document.addEventListener('click', function(e){
+  //     event.preventDefault()
+  //   const likeBtn = document.querySelector('.like-btn')
+  //   console.log('clicked!')
+  // })
+  document.addEventListener('click', function(e){
+    event.preventDefault()
+    if (e.target.className === 'like-btn'){
+      let card = e.target.parentElement
+      let p = card.querySelector('p')
+      let count = parseInt(p.textContent)
+      let toyDiv = document.querySelector('[data-num]')
+      let id = parseInt(toyDiv.getAttribute('data-num'))
+      p.textContent = count + 1
+      count = p.textContent
+      fetch(`http://localhost:3000/toys/${id}`, {
+        method: 'PATCH',
+        headers: 
+        {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          "likes": count
+        })
+      })
+    }
+  })
+})
+
